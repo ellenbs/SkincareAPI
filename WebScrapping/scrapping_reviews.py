97fa37a350d324a920f77431ccf0094410a4f0d0
ellenbs <ellenbs@al.insper.edu.br>
2024-08-31 21:30:25 -0300

    DataBase

--- WebScrapping/scrapping_reviews.py
+++ WebScrapping/scrapping_reviews.py
@@ -15,11 +15,7 @@ service = Service('./chromedriver')  # Substitua com o caminho do seu ChromeDriv
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Abrindo o site da Sephora no produto desejado
-driver.get('https://www.sephora.com.br/creme-hidratante-shiseido-vital-perfection-uplifting-firming-cream-9090740945-9090740945.html')
-
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+driver.get('https://www.sephora.com.br/tonico-rose-inc-skin-resolution-clarifying-toner-9090640948-9090640948.html')
 
 # Pausa para garantir que a página carregue completamente
 time.sleep(5)
@@ -27,13 +23,16 @@ time.sleep(5)
 # Encontrando e extraindo os reviews
 marca = driver.find_element(By.XPATH, '//*[@id="product-content"]/div[1]/div/h2/a')
 nome = driver.find_element(By.XPATH, '//*[@id="product-content"]/div[1]/div/div[3]/div/h1')
-reviews = driver.find_elements(By.XPATH, '//*[@id="BVRRContainer"]/div/div/div/div/ol/li/div[2]/div[1]/div/div[2]/div/div/div[1]/p')
+reviews_title = driver.find_elements(By.XPATH, '//*[@id="BVRRContainer"]/div/div/div/div/ol/li[1]/div[2]/div[1]/div/div[1]/div/div[2]/h3')
+reviews_text = driver.find_elements(By.XPATH, '//*[@id="BVRRContainer"]/div/div/div/div/ol/li/div[2]/div[1]/div/div[2]/div/div/div[1]/p')
 
 #printar o texto do review
 print(marca.text)
 print(nome.text)
-for review in reviews:
-    print(review.text)
+for review_title in reviews_title:
+    print(review_title.text)
+for review_text in reviews_text:
+    print(review_text.text)
 
 # Adcionando os reviews em um arquivo CSV sem apagar os anteriores
 
@@ -41,8 +40,10 @@ import csv
 
 with open('sephora_reviews.csv', 'a', newline='') as file:
     writer = csv.writer(file)
-    for review in reviews:
-        writer.writerow([marca.text, nome.text, review.text])
+    for review_title in reviews_title:
+        writer.writerow([marca.text, nome.text, review_title.text])
+    for review_text in reviews_text:
+            writer.writerow([marca.text, nome.text, review_text.text])
 
 # Fechando o WebDriver
 driver.quit()
