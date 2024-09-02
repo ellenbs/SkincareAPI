cf9fb6490e640604a75832db2c36bc7f0ded672f
ellenbs <ellenbs@al.insper.edu.br>
2024-09-02 18:05:55 -0300

    changing port

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ def query_route(query: str = Query(..., description="Search query")):
     return {"results": results, "message": "OK"}
 
 def run():
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=4524, reload=True)
 
 if __name__ == "__main__":
     run()
