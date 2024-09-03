74591913c42ce701690895fab055013361f852df
ellenbs <ellenbs@al.insper.edu.br>
2024-09-03 16:12:04 -0300

    tests passing

--- test/test_query.py
+++ test/test_query.py
@@ -3,12 +3,12 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_query_yields_10_results():
+def test_query_yields_more_10_results():
     response = client.get("/query?query=acne")
     json_response = response.json()
     
     assert response.status_code == 200
-    assert len(json_response["results"]) == 10
+    assert len(json_response["results"]) > 10
     assert json_response["message"] == "OK"
 
 def test_query_yields_few_results():
@@ -25,9 +25,9 @@ def test_query_yields_non_obvious_results():
     
     assert response.status_code == 200
     assert len(json_response["results"]) > 0
-    assert json_response["results"][0]["title"] == "SÉRUM ILUMINADOR ANTIMANCHAS CAUDALIE VINOPERFECT ALTERNATIVA À VITAMINA C"
-    assert json_response["results"][1]["title"] == "ESPUMA DE LIMPEZA SHISEIDO DEEP CLEANSING FOAM"
-    assert json_response["results"][2]["title"] == "ESSÊNCIA DE TRATAMENTO CLARINS EXTRA-FIRMING"
-    assert json_response["results"][3]["title"] == "SÉRUM HIDRATANTE DRUNK ELEPHANT B-HYDRA INTENSIVE HYDRATION SERUM"
-    assert json_response["results"][3]["title"] == "CREME HIDRATANTE FACIAL DRUNK ELEPHANT PROTINI POLYPEPTIDE CREAM"
+    assert json_response["results"][0]["Produto"] == "SÉRUM ILUMINADOR ANTIMANCHAS CAUDALIE VINOPERFECT ALTERNATIVA À VITAMINA C"
+    assert json_response["results"][1]["Produto"] == "ESPUMA DE LIMPEZA SHISEIDO DEEP CLEANSING FOAM"
+    assert json_response["results"][2]["Produto"] == "ESSÊNCIA DE TRATAMENTO CLARINS EXTRA-FIRMING"
+    assert json_response["results"][3]["Produto"] == "SÉRUM HIDRATANTE DRUNK ELEPHANT B-HYDRA INTENSIVE HYDRATION SERUM"
+    assert json_response["results"][4]["Produto"] == "CREME HIDRATANTE FACIAL DRUNK ELEPHANT PROTINI POLYPEPTIDE CREAM"
     assert json_response["message"] == "OK"
